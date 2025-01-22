656de57fb13d170396ca1e7c9ae2c5099b0e9e3b
Jasper De Landsheere <e12228451@student.tuwien.ac.at>
2025-01-22 11:22:38 +0100

    refactor: att layer base get rid of channels

--- src/deeprxn/layer/att_layer/att_layer_base.py
+++ src/deeprxn/layer/att_layer/att_layer_base.py
@@ -6,10 +6,8 @@ from torch_geometric.data import Batch
 
 class AttLayer(nn.Module):
 
-    def __init__(self, in_channels: int, out_channels: int):
+    def __init__(self):
         super().__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
 
     @abstractmethod
     def forward(self, batch: Batch) -> Batch:
