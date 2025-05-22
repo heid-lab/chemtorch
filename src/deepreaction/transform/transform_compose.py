8029785a29157f0d96838e0c9773b16df9786a9c
Anton Zamyatin <anton.zamyatin@tuwien.ac.at>
2025-05-22 16:27:41 +0200

    :bug: Rename Compose class to TransformCompose and change method name from forward to __call__

--- src/deepreaction/transform/transform_compose.py
+++ src/deepreaction/transform/transform_compose.py
@@ -4,11 +4,11 @@ from deepreaction.transform.abstract_transform import AbstractTransform
 
 T = TypeVar("T")
 
-class Compose(AbstractTransform[T]):
+class TransformCompose(AbstractTransform[T]):
     def __init__(self, transforms: Sequence[AbstractTransform[T]]):
         self.transforms = transforms
 
-    def forward(self, data: T) -> T:
+    def __call__(self, data: T) -> T:
         for t in self.transforms:
             data = t(data)
         return data
