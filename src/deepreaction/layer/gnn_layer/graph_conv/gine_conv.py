0f834a7ba856601207ad728ecb730d43b7e58887
Anton Zamyatin <anton.zamyatin@tuwien.ac.at>
2025-06-11 19:31:24 +0200

    :wrench: Update GINE configs and fix name shadowing with PyG implementation

--- src/deepreaction/layer/gnn_layer/graph_conv/gine_conv.py
+++ src/deepreaction/layer/gnn_layer/graph_conv/gine_conv.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch_geometric.nn as pyg_nn
-from torch_geometric.nn import Linear as Linear_pyg
+from torch_geometric.nn import Linear as pyg_Linear
+from torch_geometric.nn import GINEConv as pyg_GINEConv
 
 
 class GINEConv(nn.Module):
@@ -13,11 +14,11 @@ class GINEConv(nn.Module):
     ):
         super(GINEConv, self).__init__()
         gin_nn = nn.Sequential(
-            Linear_pyg(in_channels, out_channels),
+            pyg_Linear(in_channels, out_channels),
             nn.ReLU(),
-            Linear_pyg(out_channels, out_channels),
+            pyg_Linear(out_channels, out_channels),
         )
-        self.model = pyg_nn.GINEConv(gin_nn)
+        self.model = pyg_GINEConv(gin_nn)
 
     def forward(self, batch):
         batch.x = self.model(batch.x, batch.edge_index, batch.edge_attr)
