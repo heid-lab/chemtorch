1be76666792c0af5fdcd18a29fdbe8cbe520d41b
Anton Zamyatin <anton.zamyatin@tuwien.ac.at>
2025-06-16 17:35:27 +0200

    :bug: Add back linear encoder class fixing issue #35; Add docstrings to all linear encoders

--- src/deepreaction/encoder/linear_node_enc.py
+++ src/deepreaction/encoder/linear_node_enc.py
@@ -3,13 +3,24 @@ from torch_geometric.data import Batch
 
 
 class LinearNodeEncoder(nn.Module):
-
+    """
+    Linear encoder for node features.
+    This encoder applies a linear transformation to node features
+    to project them into a specified hidden space.
+    """
     def __init__(
         self,
         in_channels: int,
         out_channels: int,
         bias: bool = True,
     ):
+        """
+        Initializes the LinearNodeEncoder.
+        Args:
+            in_channels (int): Number of input node features.
+            out_channels (int): Number of output channels for the linear transformation.
+            bias (bool): Whether to include a bias term in the linear transformation.
+        """
         super(LinearNodeEncoder, self).__init__()
 
         self.encoder = nn.Linear(in_channels, out_channels, bias=bias)
