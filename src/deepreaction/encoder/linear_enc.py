01608fac6f1c1a3acd290c4f435f63f172cbb249
Jasper De Landsheere <e12228451@student.tuwien.ac.at>
2025-06-23 17:36:49 +0200

    :sparkles: allow for multiple encoders

--- src/deepreaction/encoder/linear_enc.py
+++ src/deepreaction/encoder/linear_enc.py
@@ -1,8 +1,9 @@
 from torch import nn
 from torch_geometric.data import Batch
 
-from deepreaction.encoder.linear_node_enc import LinearNodeEncoder
 from deepreaction.encoder.linear_edge_enc import LinearEdgeEncoder
+from deepreaction.encoder.linear_node_enc import LinearNodeEncoder
+
 
 class LinearEncoder(nn.Module):
     """
@@ -10,38 +11,36 @@ class LinearEncoder(nn.Module):
     This encoder applies a linear transformation to both node and edge features
     to project them into a common hidden space.
     """
+
     def __init__(
         self,
-        num_node_features: int,
-        num_edge_features: int,
+        node_encoder_in_channels: int,
+        edge_encoder_in_channels: int,
         out_channels: int,
         bias: bool = True,
     ):
         """
         Initializes the LinearEncoder.
         Args:
-            num_node_features (int): Number of input node features.
-            num_edge_features (int): Number of input edge features.
+            node_encoder_in_channels (int): Number of input node features.
+            edge_encoder_in_channels (int): Number of input edge features.
             out_channels (int): Number of output channels for the linear transformation.
             bias (bool): Whether to include a bias term in the linear transformations.
         """
         super(LinearEncoder, self).__init__()
 
         self.node_encoder = LinearNodeEncoder(
-            in_channels=num_node_features,
+            in_channels=node_encoder_in_channels,
             out_channels=out_channels,
-            bias=bias
+            bias=bias,
         )
         self.edge_encoder = LinearEdgeEncoder(
-            in_channels=num_edge_features,
+            in_channels=edge_encoder_in_channels,
             out_channels=out_channels,
-            bias=bias
+            bias=bias,
         )
 
     def forward(self, batch: Batch) -> Batch:
         batch = self.node_encoder(batch)
         batch = self.edge_encoder(batch)
         return batch
-
-
-
