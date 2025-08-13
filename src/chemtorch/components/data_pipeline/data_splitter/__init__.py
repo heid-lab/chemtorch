b5d5995ec2389410cbf7347317bdeed0c226c041
Jasper De Landsheere <e12228451@student.tuwien.ac.at>
2025-08-13 11:02:33 +0200

    :sparkles: Target splitting

--- src/chemtorch/components/data_pipeline/data_splitter/__init__.py
+++ src/chemtorch/components/data_pipeline/data_splitter/__init__.py
@@ -2,3 +2,4 @@ from .data_splitter import DataSplitter
 from .index_splitter import IndexSplitter
 from .ratio_splitter import RatioSplitter
 from .size_splitter import SizeSplitter
+from .target_splitter import TargetSplitter
