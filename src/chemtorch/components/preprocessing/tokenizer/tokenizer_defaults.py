e2cd1c2711d38f5c1f2735c427a7d4cad31963c6
Anton Zamyatin <anton.zamyatin@tuwien.ac.at>
2025-08-27 16:52:44 +0200

    :hammer: Clean simple tokenization regex of unnecessary symbols

--- src/chemtorch/components/preprocessing/tokenizer/tokenizer_defaults.py
+++ src/chemtorch/components/preprocessing/tokenizer/tokenizer_defaults.py
@@ -1,4 +1,19 @@
-DEFAULT_MOLECULE_PATTERN = r"(\%\([0-9]{3}\)|\[[^\]]+]|Br?|Cl?|N|O|S|P|F|I|b|c|n|o|s|p|\||\(|\)|\.|=|#|-|\+|\\|\/|:|~|@|\?|>>?|\*|\$|\%[0-9]{2}|[0-9])"
-DEFAULT_UNK_TOKEN = "<UNK>"
+DEFAULT_MOLECULE_PATTERN = (
+    r"("
+    r"\[[^\]]+]|"           # Bracketed expressions: [CH3], [OH], [N+]
+    r"Br?|Cl?|"             # Bromine/Chlorine (with optional second letter)
+    r"N|O|S|P|F|I|"         # Single uppercase atoms
+    r"b|c|n|o|s|p|"         # Single lowercase atoms
+    r"\(|\)|\.|"            # Structural symbols: |, (, ), .
+    r"-|=|#|:|"             # Bond symbols: single (-), double (=), triple (#), aromatic (:)
+    r"\\|\/|\+|@|"          # Special symbols: configuration around double bonds (\ and /), sterics (@ or @@)
+    r">>?|"                 # Reaction arrows: > or >>
+    r"~|\*|"                # Wildcards: any bond (~), any atom (*)
+    r"[0-9]"                # Single digits for ring closure
+    r"\%[0-9]{2}|"          # Percent with 2 digits for ring closure with more than 9 rings open at once (extremely rare)
+    r")"
+)
+DEFAULT_UNK_TOKEN = "[UNK]"
+DEFAULT_PAD_TOKEN = "[PAD]"
 REACTION_SEPARATOR_TOKEN = ">>"
 MOLECULE_SEPARATOR_TOKEN = "."
